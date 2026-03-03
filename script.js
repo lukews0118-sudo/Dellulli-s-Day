8e40fe8c693f593590bd681df43aff2db6813004
lukews0118-sudo <lukews0118@gmail.com>
2026-03-02 22:42:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ if (playButton) {
         window.location.href = "video.html";
     });
 }
+
+const video = document.getElementById("introVideo");
+const nextButton = document.getElementById("nextButton");
+
+if (video) {
+    video.addEventListener("ended", function() {
+        nextButton.style.display = "block";
+    });
+}
+
+if (nextButton) {
+    nextButton.addEventListener("click", function() {
+        window.location.href = "bedroom.html"; 
+    });
+}
